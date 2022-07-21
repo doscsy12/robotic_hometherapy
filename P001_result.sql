991b6594dcaddbdea34ae7a9548176857f8c8159
Sook-Yee Chong <51235784+doscsy12@users.noreply.github.com>
2022-07-21 15:29:50 +0800

    Update sql

--- P001_result.sql
+++ P001_result.sql
@@ -1,4 +1,5 @@
 SELECT PatientID, SessionID, GameID, TimeStampDate, TimeStampMS, Sparc, VisualTargetXCoord, VisualTargetYCoord, TargetXCoord, TargetYCoord, XCoordinate, YCoordinate, KxGain, KyGain, KxyGain, BxGain, ByGain, BxyGain, ByxGain, VelocityX, VelocityY, CurrentSenseLeft, CurrentSenseRight, TotalForce
 FROM TargetFeedbackDataModel
 -- change PatientID accordingly 
-WHERE PatientID='TTSH-CHISEL0Pa/Gtc3DUqoPVREp+I/qg=='
+-- change start date of home-use 
+WHERE PatientID='TTSH-CHISEL0Pa/Gtc3DUqoPVREp+I/qg==' AND TimeStampDate >= '2022-03-11' 
